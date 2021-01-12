e7a53063957589fc53b998626499cd8129109c02
wssy001 <peter003@foxmail.com>
2021-01-12 14:58:11 +0800

    更新

---
+++ src/main/java/cyou/wssy001/banish/controller/TestController.java
@@ -0,0 +1,15 @@
+package cyou.wssy001.banish.controller;
+
+import cn.hutool.http.HttpUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return HttpUtil.get("https://www.baidu.com", StandardCharsets.UTF_8);
+    }
+}
